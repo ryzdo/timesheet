1bd82eb7d2a5fa24db57498a9b926e6688d51c17
Denis Ryzhankov <denryzhankov@gmail.com>
2025-03-24 16:08:23 +0300

    Squashed commit of the following:
    commit 3744f190fbc3163bce4e94d26e94578d45f07e23
    Author: Denis Ryzhankov <denryzhankov@gmail.com>
    Date:   Mon Mar 24 07:32:52 2025 +0300
    
        add postgresql connection
    
    commit 8663b148f4f9136047dcd6aece97ac28242686dd
    Author: Denis Ryzhankov <denryzhankov@gmail.com>
    Date:   Mon Mar 24 07:29:52 2025 +0300
    
        add test connection to postgresql
    
    commit 13b720e95d044d7e03abfe8f985785c759ed6c7e
    Author: Denis Ryzhankov <denryzhankov@gmail.com>
    Date:   Mon Mar 24 07:25:23 2025 +0300
    
        add pytest_databases in dev dependency

--- src/app.py
+++ src/app.py
@@ -1,4 +1,8 @@
 from litestar import Litestar, MediaType, get
+from litestar.plugins.sqlalchemy import SQLAlchemyAsyncConfig, SQLAlchemyPlugin
+
+from src.config import get_postgres_uri
+from src.filling.adapters.orm import mapper_registry
 
 
 @get(path="/health-check", media_type=MediaType.TEXT)
@@ -6,4 +10,15 @@ async def health_check() -> str:
     return "healthy"
 
 
-app = Litestar(route_handlers=[health_check])
+db_config = SQLAlchemyAsyncConfig(
+    connection_string=get_postgres_uri(),
+    metadata=mapper_registry.metadata,
+    create_all=True,
+    before_send_handler="autocommit",
+)
+
+
+app = Litestar(
+    route_handlers=[health_check],
+    plugins=[SQLAlchemyPlugin(db_config)],
+)
