9be06398ad31dc259160550dabf85438f9f72507
Denis Ryzhankov <denryzhankov@gmail.com>
2025-04-13 04:26:22 +0300

    Add WorkDayController in app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
+from collections.abc import Mapping
+from datetime import date
 from typing import Any
 
-from litestar import Litestar, MediaType, get
+from litestar import Controller, Litestar, MediaType, get, post
+from litestar.di import Provide
+from litestar.logging import LoggingConfig
 from litestar.plugins.sqlalchemy import SQLAlchemyAsyncConfig, SQLAlchemyPlugin
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,6 +12,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.config import get_postgres_uri
 from src.filling.adapters.orm import mapper_registry, start_mappers
 from src.filling.domain import model
+from src.filling.service_layer.services import WorkDayService, provide_work_days_service
 
 
 WorkDayType = dict[str, Any]
@@ -38,6 +43,36 @@ db_config = SQLAlchemyAsyncConfig(
 )
 
 
+class WorkDayController(Controller):
+    """Author CRUD endpoints."""
+
+    path = "/api/v2/workdays"
+    dependencies: Mapping[str, Provide] = {"work_days_service": Provide(provide_work_days_service)}
+
+    @get()
+    async def get_list_work_days2(self, work_days_service: WorkDayService) -> list[WorkDayType]:
+        return [serialize(wd) for wd in await work_days_service.list()]
+
+    @post()
+    async def create_work_day(
+        self,
+        work_days_service: WorkDayService,
+        date: date,
+    ) -> date:
+        """Create a new author."""
+        obj = await work_days_service.create({"date": date})
+        return obj.date
+
+
+logging_config = LoggingConfig(
+    root={"level": "INFO", "handlers": ["queue_listener"]},
+    formatters={"standard": {"format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"}},
+    log_exceptions="always",
+)
+
 app = Litestar(
-    route_handlers=[health_check, get_list_work_days], plugins=[SQLAlchemyPlugin(db_config)], on_startup=[start_mappers]
+    route_handlers=[health_check, get_list_work_days, WorkDayController],
+    plugins=[SQLAlchemyPlugin(db_config)],
+    on_startup=[start_mappers],
+    logging_config=logging_config,
 )
